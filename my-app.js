ea86ad1e6ce3f34c00e5da5a40591883d289f3d7
Marcincin9 <marcino@gmx.com>
2022-08-30 19:06:20 +0100

    add function to create new book cards

--- my-app.js
+++ my-app.js
@@ -57,6 +57,40 @@ function displayBooks() {
     makeCard(myLibrary[myLibrary.length - 1]);
 }
 
+// function to create new book cards
+
+function makeCard(bookToAdd)
+{
+    let card = document.createElement('div');
+    let title = document.createElement('h1');
+    let author = document.createElement('h2');
+    let pages = document.createElement('p');
+    let removeButton = document.createElement('BUTTON');
+    
+    isRead.className = 'isReadStatus';
+    readStatusBtn.className = 'readStatusBtn';
+    readStatusBtn.innerText = 'Change Read Status';
+    readStatusBtn.setAttribute("data-index", myLibrary.indexOf(bookToAdd));
+    removeButton.className = 'removeButton';
+    removeButton.innerText = 'Remove Book';
+    removeButton.setAttribute("data-index", myLibrary.indexOf(bookToAdd));
+    card.classList.add('card');
+
+    title.textContent = bookToAdd.title;
+    author.textContent = bookToAdd.author;
+    pages.textContent = `${bookToAdd.pages} pages`;
+    isRead.textContent = bookToAdd.isRead;
+
+    card.append(title);
+    card.append(author);
+    card.append(pages);
+    card.append(isRead);
+    card.append(removeButton);
+    card.append(readStatusBtn);
+    card.setAttribute("data-index", myLibrary.indexOf(bookToAdd));
+    container.append(card);
+}
+
 // open and close form window
 
 function openForm() {
